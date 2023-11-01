435364664f112d0f72628435cc9bfb155a9d4699
Çağan <cagangdk6@gmail.com>
2023-11-01 21:28:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include<locale.h>
+
+
+
+int main(){
+setlocale(LC_ALL, "Turkish");
+int a,bas,kal,i=0,deger=1,yedek;
+
+printf("Sayiyi girin:");
+scanf("%d",&a);
+yedek=a;
+printf("Kaç basamak ayýrmak istiyorsunuz:");
+scanf("%d",&bas);
+for(; i<bas;  i++){
+	kal=a%10;
+	a=a/10;
+	if(i==0){
+		printf("1'ler/lar basamaðý=%d\t",kal);
+	}
+	
+	else{
+	printf("%d'ler/lar basamaðý=%d\t",deger,kal);
+	}
+	
+	deger=deger*10;
+	if(deger>yedek){
+		break;
+	}
+	
+	
+	}
+
+}
